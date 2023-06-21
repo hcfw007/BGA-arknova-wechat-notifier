24ecc95c0f72aaa196f652b8980daa742e65d098
NickWang <brotherstyx@gmail.com>
2023-06-21 11:43:55 +0800

    feat: 🎸 basic event listner

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { WechatyBuilder } from '@juzi/wechaty'
+import { WechatyBuilder, log } from '@juzi/wechaty'
 import { ScanStatus } from '@juzi/wechaty-puppet/types'
 import QrcodeTerminal from 'qrcode-terminal'
 import PuppetPadplus from '@juzi/wechaty-puppet-padplus'
@@ -7,6 +7,9 @@ import {
   token,
   endpoint,
 } from './config'
+import { ContactInterface, MessageInterface } from '@juzi/wechaty/impls'
+
+const PRE = 'Index'
 
 const puppet = new PuppetPadplus({
   token,
@@ -26,6 +29,14 @@ bot.on('scan', (qrcode: string, status: ScanStatus) => {
       small: true
     })
   }
+}).on('login', (contact: ContactInterface) => {
+  log.info(PRE, `user login, info: ${JSON.stringify(contact)}`)
+}).on('ready', () => {
+  log.info(PRE, `data ready, start listening`)
+}).on('message', (message: MessageInterface) => {
+  log.info(PRE, `message received, payload: ${JSON.stringify(message.payload)}`)
+}).on('error', (error: Error) => {
+  log.error(PRE, `error: ${error.stack}}`)
 })
 
 bot.start()
